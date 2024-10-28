1cba9fb5a8f269c9952f1c8bba4f158a05e3dd7f
schetti92 <schetti92@gmail.com>
2024-10-29 09:52:20 +1100

    Initial commit

---
+++ SPFX_CURD/src/webparts/wpspfxCurd/components/IWpspfxCurdProps.ts
@@ -0,0 +1,6 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+
+export interface IWpspfxCurdProps {
+  listName: string;
+  context: WebPartContext;
+}
---
+++ SPFX_CURD/src/webparts/wpspfxCurd/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IWpspfxCurdWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  ListNameLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'WpspfxCurdWebPartStrings' {
+  const strings: IWpspfxCurdWebPartStrings;
+  export = strings;
+}
